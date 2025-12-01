cf395885df7c8c68d781c76bbb939e3d8e5fe5df
a2021145193@isec.pt <a2021145193@isec.pt>
2025-12-01 16:47:23 +0000

    Ultimos ajustes Pre-API

--- GestaoLoja/GestaoLoja/GestaoLoja/AppConfig.cs
+++ GestaoLoja/GestaoLoja/GestaoLoja/AppConfig.cs
@@ -1,9 +1,13 @@
 ﻿namespace GestaoLoja
 {
-    public class AppConfig
+    public static class AppConfig
     {
-        public static readonly string TituloHomePage = "MyCOLL";
-        public static readonly string EnderecoLoja = "Rua dos Colecionadores, Coimbra";
-        public static readonly string EmailGeral = "geral@mycoll.pt";
+        public static readonly string NomeLoja = "MyCOLL";
+        public static readonly string TituloBackoffice = "MyCOLL Gestão";
+        public static readonly string Email = "geral@mycoll.pt"; 
+        public static readonly string Telefone = "291 987 123";
+        public static readonly string Morada = @"R. Padre Luís Miguel nº 79,
+Santo Antonio dos Olivais,
+3030-243, Coimbra";
     }
 }
