cc602cca51258c45b804601650fefb7bb833df57
a2021145193@isec.pt <a2021145193@isec.pt>
2025-12-26 21:09:32 +0000

    .

---
+++ FrontEnd/RCLImages/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RCLImages.MyFeature.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
